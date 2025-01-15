a2ebb10aa98f9b7ea58be07b4e62a1b71456e27e
bbirlie <bbirie@hotmail.com>
2025-01-14 20:33:26 -0800

    pagination and such

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -2,14 +2,19 @@
     <x-slot:heading>
         Jobs Page
     </x-slot:heading>
-    <h1>Jobs</h1>
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <a href="/jobs/{{$job['id']}}">
-                <li>
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-grey-200 rounded">
+                <div class="font-bold text-blue-500">
+                    {{$job->employer->name}}
+                </div>
+                <div>
                     <p><strong>{{$job['title']}}:</strong> makes {{$job['salary']}} per year.</p>
-                </li>
+                </div>
             </a>  
         @endforeach
-    </ul>
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => Job::all(),
+        'jobs' => Job::with('employer')->paginate(3),
     ]);
 });
 
